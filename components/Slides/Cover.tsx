e5ffa236d0bc5f9069b131ec8a5f94d602956e42
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2023-04-17 17:52:36 +0530

    Cover: base class added

--- components/Slides/Cover.tsx
+++ components/Slides/Cover.tsx
@@ -1,5 +1,7 @@
 import * as React from "react"
 
+import classNames from "utils/classNames"
+
 interface Props {
   title: string
   subtitle?: string
@@ -8,8 +10,8 @@ interface Props {
 
 const Cover: React.FC<Props> = ({ title, subtitle, className }) => {
   return (
-    <div className={className}>
-      <h1 className="text-center text-3xl font-bold leading-normal text-gray-900">
+    <div className={classNames("text-center", className)}>
+      <h1 className="text-3xl font-bold leading-normal text-gray-900">
         {title}
       </h1>
       {subtitle !== undefined && (
