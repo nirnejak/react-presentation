0e2e051d2207c3d27771783657ee3c0b504c66e1
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2023-04-17 17:27:32 +0530

    Cover: typography improved, container classnames prop added

--- components/Slides/Cover.tsx
+++ components/Slides/Cover.tsx
@@ -3,14 +3,17 @@ import * as React from "react"
 interface Props {
   title: string
   subtitle?: string
+  className?: string
 }
 
-const Cover: React.FC<Props> = ({ title, subtitle }) => {
+const Cover: React.FC<Props> = ({ title, subtitle, className }) => {
   return (
-    <div>
-      <h1 className="text-center text-3xl font-bold text-gray-900">{title}</h1>
+    <div className={className}>
+      <h1 className="text-center text-3xl font-bold leading-normal text-gray-900">
+        {title}
+      </h1>
       {subtitle !== undefined && (
-        <p className="mt-4 text-gray-500">{subtitle}</p>
+        <p className="mt-4 leading-normal text-gray-500">{subtitle}</p>
       )}
     </div>
   )
