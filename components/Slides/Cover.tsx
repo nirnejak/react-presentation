f690230bc80cd4cc20bc84bac17a8b511c1f7077
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2023-04-17 18:59:19 +0530

    Cover: reveal animation added

--- components/Slides/Cover.tsx
+++ components/Slides/Cover.tsx
@@ -1,4 +1,7 @@
 import * as React from "react"
+import { useInView } from "react-intersection-observer"
+
+import { motion, useAnimation } from "framer-motion"
 
 import classNames from "utils/classNames"
 
@@ -9,15 +12,38 @@ interface Props {
 }
 
 const Cover: React.FC<Props> = ({ title, subtitle, className }) => {
+  const controls = useAnimation()
+  const [ref, inView] = useInView()
+
+  React.useEffect(() => {
+    if (inView) {
+      controls.start("visible").catch((err) => {
+        console.log(err)
+      })
+    }
+  }, [controls, inView])
+
+  const variants = {
+    visible: { opacity: 1 },
+    hidden: { opacity: 0 },
+  }
+
   return (
-    <div className={classNames("text-center", className)}>
+    <motion.div
+      animate={controls}
+      initial="hidden"
+      transition={{ delay: 0, duration: 0.15, type: "spring" }}
+      variants={variants}
+      ref={ref}
+      className={classNames("text-center", className)}
+    >
       <h1 className="text-3xl font-bold leading-normal text-gray-900">
         {title}
       </h1>
       {subtitle !== undefined && (
         <p className="mt-4 leading-normal text-gray-500">{subtitle}</p>
       )}
-    </div>
+    </motion.div>
   )
 }
 
