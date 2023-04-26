d248ed1020beb2ee41c74c29ee6d9945794f6bb8
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2023-04-26 18:39:08 +0530

    Wrapper minor refactoring

--- components/Wrapper.tsx
+++ components/Wrapper.tsx
@@ -29,11 +29,11 @@ const Wrapper: React.FC<Props> = ({ children, className }) => {
 
   return (
     <motion.div
-      animate={controls}
-      initial="hidden"
-      transition={{ delay: 0, duration: 0.4, type: "spring" }}
-      variants={variants}
       ref={ref}
+      initial="hidden"
+      animate={controls}
+      variants={variants}
+      transition={{ delay: 0, duration: 0.4, type: "spring" }}
       className={classNames("w-[680px]", className)}
     >
       {children}
