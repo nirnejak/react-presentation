30fa788068fe137211cf4fc74d4a1846c87ee25f
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2023-04-26 18:38:40 +0530

    Wrapper used in demo component

--- components/demo/Buttons.tsx
+++ components/demo/Buttons.tsx
@@ -1,28 +1,32 @@
 import * as React from "react"
 
+import Wrapper from "components/Wrapper"
+
 const Buttons: React.FC = () => {
   const [count, setCount] = React.useState(0)
 
   return (
-    <div className="flex items-center gap-4">
-      <button
-        className="select-none rounded-md bg-gray-800 px-5 py-3 text-gray-200 transition-all hover:bg-gray-900 active:scale-95"
-        onClick={() => {
-          setCount(count - 1)
-        }}
-      >
-        -
-      </button>
-      <p>Count is {count}</p>
-      <button
-        className="select-none rounded-md bg-gray-800 px-5 py-3 text-gray-200 transition-all hover:bg-gray-900 active:scale-95"
-        onClick={() => {
-          setCount(count + 1)
-        }}
-      >
-        +
-      </button>
-    </div>
+    <Wrapper className="flex justify-center">
+      <div className="flex items-center gap-4">
+        <button
+          className="select-none rounded-md bg-gray-800 px-5 py-3 text-gray-200 transition-all hover:bg-gray-900 active:scale-95"
+          onClick={() => {
+            setCount(count - 1)
+          }}
+        >
+          -
+        </button>
+        <p>Count is {count}</p>
+        <button
+          className="select-none rounded-md bg-gray-800 px-5 py-3 text-gray-200 transition-all hover:bg-gray-900 active:scale-95"
+          onClick={() => {
+            setCount(count + 1)
+          }}
+        >
+          +
+        </button>
+      </div>
+    </Wrapper>
   )
 }
 
