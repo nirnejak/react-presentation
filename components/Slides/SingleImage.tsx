8c4f6e800d9b66d2170b119bf158ea4b212e5afe
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2024-10-24 17:44:14 +0530

    Single Image component added

---
+++ components/Slides/SingleImage.tsx
@@ -0,0 +1,34 @@
+"use client"
+import * as React from "react"
+
+import { motion } from "framer-motion"
+
+import useFadeUp from "hooks/useFadeUp"
+import classNames from "utils/classNames"
+
+interface Props {
+  image?: string
+  className?: string
+}
+
+const SingleImage: React.FC<Props> = ({ image = [], className }) => {
+  const { ref, controls, variants } = useFadeUp()
+
+  return (
+    <div ref={ref} className={classNames("", className)}>
+      <motion.img
+        initial="hidden"
+        animate={controls}
+        variants={variants}
+        transition={{
+          delay: 0.1,
+          duration: 0.4,
+          type: "spring",
+        }}
+        src={image}
+      />
+    </div>
+  )
+}
+
+export default SingleImage
